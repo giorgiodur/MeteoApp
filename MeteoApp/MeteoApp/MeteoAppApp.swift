d9b5ebaf8c7630650d912aebfba0ee1d7a782fc5
Giorgio <gorgo.durante@gmail.com>
2025-12-16 10:25:49 +0100

    Initial project setup

---
+++ MeteoApp/MeteoApp/MeteoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoAppApp.swift
+//  MeteoApp
+//
+//  Created by Giorgio Durante on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MeteoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
